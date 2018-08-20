f4670a75e3ec04b1658704281fc2fb11336cb351
Bao Dang <baodq96@gmail.com>
2018-08-20 14:38:21 +0700

    test heroku

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import './js/state'
 import './css/style.sass';
 
 $( document ).ready(function() {
+  $.LoadingOverlay("show");
   $("#logout").on('click', () => {
-    confirm("Are you sure?");
+    confirm("Are you suree?");
     firebase.auth().signOut().then(function() {
-      // Sign-out successful.
+      location.reload();
     }, function(error) {
       // An error happened.
     });
@@ -14,6 +14,13 @@ $( document ).ready(function() {
   $("#avatar").change(function() {
     readURL(this);
   });
+
+  $( "#birthday" ).datepicker({
+   maxDate: '0',
+   dateFormat: 'dd/mm/yy',
+   }).on('change', function() {
+  });
+
 
 });
 
@@ -43,6 +50,7 @@ $( "#login-form" ).submit(function( event ) {
   });
 });
 
+$("#dashboard-form").submit(e => e.preventDefault());
 $("#dashboard-form").validate({
   rules: {
     birthday: {
@@ -54,49 +62,98 @@ $("#dashboard-form").validate({
   },
   submitHandler: function(form) {
     $.LoadingOverlay("show", {
-      image       : "",
-      progress    : true,
-      progressColor: "#7FDBFF"
+      text: "Update Information"
     });
-      // var form_fields = ['username', 'first_name', 'last_name', 'birthday', 'gender', 'phone', 'address']
-      // var data = {};
-    // for(var i=0; i < form_fields.length; i++){
-    //   data[form_fields[i]] = $('#' + form_fields[i]).val()
-    // }
-    // Create a root reference
-    var storageRef = firebase.storage().ref();
+    let user = firebase.auth().currentUser;
+    var form_fields = ['username', 'first_name', 'last_name', 'birthday', 'gender', 'phone', 'address']
+    let data = {}
+    form_fields.forEach( x => {
+      return data[x] = $('#' + x).val();
+    })
+    firebase.database().ref('users/' + user.uid).set(data);
+    fill_form_dashboard(user.uid);
     var file = $('#avatar').get(0).files[0];
 
-    let user = firebase.auth().currentUser;
-    var uploadTask = storageRef.child(`/${user}/${user.uid}/avatar`).put(file);
-    uploadTask.on('state_changed', function(snapshot){
-      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    }, function(error) {
-      // Handle unsuccessful uploads
-    }, function() {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      $.LoadingOverlay("hide");
-      uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-        $('#profilePicture').attr('src', downloadURL)
-        user.updateProfile({
-          photoURL: downloadURL
-        }).then(function() {
-          // Update successful.
-        }).catch(function(error) {
-          // An error happened.
-        });
+    $.LoadingOverlay("hide");
+    if(file) {
+      $.LoadingOverlay("show", {
+        image       : "",
+        progress    : true,
+        text: "Uploading Image",
+        progressColor: "#7FDBFF"
+      });
+      var storageRef = firebase.storage().ref();
+      var uploadTask = storageRef.child(`/${user}/${user.uid}/avatar`).put(file);
+      uploadTask.on('state_changed', function(snapshot){
+        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        $.LoadingOverlay("progress", progress);
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+        // Handle unsuccessful uploads
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        $.LoadingOverlay("hide");
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+          $('#profilePicture').attr('src', downloadURL)
+          user.updateProfile({
+            photoURL: downloadURL
+          }).then(function() {
+            toastr["success"]("Update Success")
+          }).catch(function(error) {
+            // An error happened.
+            toastr["error"]("Something went wrong :(")
+          });
 
+        });
+      });
+    }
+  }
+})
+
+$("#sign-up-form").validate({
+  messages: {
+    'sign-up-password-confirm': {
+      equalTo: "Password not match",
+    }
+  },
+  rules: {
+    'sign-up-password': {
+      required: true,
+      minlength: 6
+    },
+    'sign-up-password-confirm': {
+      equalTo: "#sign-up-password",
+      required: true,
+      minlength: 6
+    },
+    'sign-up-email': {
+      required: true,
+      email: true
+    }
+  },
+  submitHandler: function(form) {
+    $( "#sign-up-form" ).submit(function( event ) {
+      $.LoadingOverlay('show');
+      event.preventDefault();
+      var myemail = $('#sign-up-email').val();
+      var mypassword = $('#sign-up-password').val();
+      firebase.auth().createUserWithEmailAndPassword(myemail, mypassword).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if(errorMessage !== ''){
+          $.LoadingOverlay('hide');
+          toastr["error"](errorMessage)
+        }
       });
     });
   }
-})
+});
--- src/js/state.js
+++ src/js/state.js
@@ -1,18 +1,34 @@
+function fill_form_dashboard(id){
+  var form_fields = ['username', 'first_name', 'last_name', 'birthday', 'gender', 'phone', 'address']
+  firebase.database().ref('/users/' + id).once('value').then(function(snapshot) {
+    form_fields.forEach( x => $('#' + x).val(snapshot.val()[x]))
+  });
+}
+
+function logged_in_layout(){
+  $('#dashboard-section').show();
+  $('#signup-div').hide();
+  $('#login-div').hide();
+  $('#hello-user').show()
+  $('#currentUser').show().text('Hello ' + name)
+}
+
+function logged_out_layout(){
+  $('#dashboard-section').hide();
+  $('#login-div').show();
+  $('#signup-div').show();
+  $('#currentUser').hide();
+  $('#hello-user').hide();
+}
 firebase.auth().onAuthStateChanged(function(user) {
   $.LoadingOverlay('hide');
   if (user) {
+    var name = user.displayName ? user.displayName : user.email;
+    if(user.photoURL) $('#profilePicture').attr('src', user.photoURL)
     console.log('current user: ', user)
-    $('#profilePicture').attr('src', user.photoURL)
-    $('#dashboard-section').show();
-    $('#signup-div').hide();
-    $('#login-div').hide();
-    name = user.displayName ? user.displayName : user.email;
-    $('#hello-user').show()
-    $('#currentUser').show().text('Hula ' + name)
+    logged_in_layout();
+    fill_form_dashboard(user.uid);
   } else {
-    $('#dashboard-section').hide();
-    $('#login-div').show();
-    $('#signup-div').show();
-    $('#hello-user').hide()
+    logged_out_layout();
   }
 });
